d63a37cdfeb3a6930599d59a1ec7ff45032d619f
Vinicius Alves <viniciusklonny@hotmail.com>
2021-08-29 13:37:55 -0300

    Criando tela de listas

---
+++ components/PanelCreateList.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="bg-white rounded shadow-md">
+    <form class="flex flex-col m-10" @submit.prevent>
+      <h1 class="text-xl text-center md:text-left mb-5">Adicione sua lista</h1>
+      <div class="mb-7 flex md:block">
+        <label for="title">
+          Title da lista
+          <input id="title" name="tilte" class="block form-input border-gray-300 shadow-md px-3 py-2 rounded focus:outline-none focus:ring-1 focus:border-blue-300" type="text">
+        </label>
+      </div>
+      <div class="mb-7 flex md:block">
+        <label for="description">
+          Descrição
+          <input type="text" name="description" id="description" class="block form-input border-gray-300 shadow-md px-3 py-2 rounded focus:outline-none focus:ring-1 focus:border-blue-300">
+        </label>
+      </div>
+      <div class="flex md:block">
+        <button
+          class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded m-1 md:m-0"
+          type="submit">Adicionar
+        </button>
+
+        <button
+          class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded m-1 md:m-0"
+          type="reset">Limpar
+        </button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/your-list/index.vue
+++ pages/your-list/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>Teste</h1>
+  <section>
+    <div class="bg-green-400 shadow-sm mb-5 md:mb-10 flex justify-center">
+      <NuxtLink to="/">
+        <h1 class="text-center text-2xl text-white p-5 italic">Your To-Do List</h1>
+      </NuxtLink>
+    </div>
+    <div class="container mx-auto flex flex-col">
+      <PanelCreateList/>
+      <PanelList/>
+    </div>
+  </section>
 </template>
-<script>
-export default {
-
-}
-</script>
