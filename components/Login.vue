ae822571be738f62476377e8f2e7ecd7ca3d5d03
Vinicius Alves <viniciusklonny@hotmail.com>
2021-08-29 12:25:52 -0300

    Ajustando tela inicial

--- components/Login.vue
+++ components/Login.vue
@@ -4,19 +4,21 @@
       <h1 class="text-center text-2xl md:text-4xl text-white p-10 italic">Your To-Do List</h1>
     </div>
 
-    <div class="p-5 flex justify-center flex-col">
-      <input
-        v-model="username"
-        class="form-input border-gray-300 shadow-md px-3 py-2 rounded focus:outline-none focus:ring-1 focus:border-blue-300"
-        type="text" name="username" id="username"
-        placeholder="Digite seu nome"
-      >
+    <form @submit.prevent="validUsername()">
+      <div class="p-5 flex justify-center flex-col">
+        <input
+          v-model="username"
+          class="form-input border-gray-300 shadow-md px-3 py-2 rounded focus:outline-none focus:ring-1 focus:border-blue-300"
+          type="text" name="username" id="username"
+          placeholder="Digite seu nome"
+        >
 
-      <button @click="printUsername()"
-        class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mt-5"
-        id="submit-username">Começar
-      </button>
-    </div>
+        <button
+          class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mt-5"
+          id="submit-username" type="submit">Começar
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -29,9 +31,16 @@ export default {
     }
   },
   methods: {
-    printUsername: function() {
-      if (this.username) {
-        Swal.fire(this.username);
+    validUsername: function() {
+      if (!this.username) {
+        Swal.fire({
+          icon: 'info',
+          title: 'Informação',
+          text: 'Por favor, informe seu nome para continuar.',
+          confirmButtonColor: '#34d399'
+        });
+      } else {
+        this.$router.push('/your-list');
       }
     }
   }
